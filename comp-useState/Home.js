e028b38229188a338fdcc789aaa5a1557284e95c
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-02-09 10:58:08 +0100

    commit

---
+++ comp-useState/Home.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const Home = () => {
+ const [name, setName] = useState('mario')
+ const [age, setAge] = useState(25)
+
+    
+    const handelClick = () => {
+        setName('luigi')
+        setAge(30)
+      
+    }
+    
+    return (
+        <div className="Home">
+        <h2>Homepage</h2>
+        <p>{name} is {age} years old</p>
+           <button onClick={handelClick}>Click Me</button> 
+        </div>
+    )
+}
+
+export default Home
--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      count:0
+    }
+  
+  this.handleClick = this.handleClick.bind(this)
+  }
+
+    handleClick() {
+      this.setState(prevState => {
+        return {
+          count:prevState.count +1
+        }
+      })
+    }
+
+    render(){
+      return (
+
+        <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>change</button>
+        
+        
+        </div>
+      )
+    }
+}
 export default App
